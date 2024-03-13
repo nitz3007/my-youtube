2446edd4d5c3e0857d9200e777520b27bb0f50d9
nitz30 <niharikat80@gmail.com>
2024-03-13 19:39:54 +0530

    removed google api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = 'AIzaSyDukPoSCRzKJ7Rh8klKTexUT3II98YsIjE';
+
 
 export const YOUTUBE_VIDEO_LIST_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=' + GOOGLE_API_KEY;
